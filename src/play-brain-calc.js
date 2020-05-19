722900ce8d3a250b192d9e904e49999cc862ada1
MYiLA <polus180@ya.ru>
2020-05-19 03:03:41 +0300

    created brain-calc

--- src/play-brain-calc.js
+++ src/play-brain-calc.js
@@ -1,10 +1,52 @@
 import createFlow from './flow.js';
 import getRandomNum from './random-num.js';
 
+const actionsArr = ['+', '-', '*'];
+
+const generateQuestion = () => {
+  const a = getRandomNum();
+  const b = getRandomNum();
+  const action = actionsArr[getRandomNum(0, actionsArr.length - 1)];
+
+  if (action === '+') {
+    return `${a} + ${b}`;
+  }
+
+  if (action === '*') {
+    return `${a} * ${b}`;
+  }
+
+  if (action === '-') {
+    return `${a} - ${b}`;
+  }
+
+  return undefined;
+};
+
+const generateAnswer = (question) => {
+  const questionArr = question.split(' ');
+  const a = Number(questionArr[0]);
+  const b = Number(questionArr[2]);
+  const action = questionArr[1];
+
+  if (action === '+') {
+    return String(a + b);
+  }
+
+  if (action === '*') {
+    return String(a * b);
+  }
+
+  if (action === '-') {
+    return String(a - b);
+  }
+
+  return undefined;
+};
+
 const playCalc = (name) => {
-  console.log(`hello, ${name}!!!!!!!!!!!`);
-  // console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  // createFlow(name, generateQuestion, generateAnswer);
+  console.log('What is the result of the expression?');
+  createFlow(name, generateQuestion, generateAnswer);
 };
 
 export default playCalc;
